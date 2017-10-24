00f4a78e1b63c8b83792d17472039d1a38cce8cd
BIBO <18516053895@163.com>
2017-10-24 19:51:20 +0800

    TPOT

--- CNN.py
+++ CNN.py
@@ -2,10 +2,12 @@
 Created by Haochuan Lu on 10/23/17.
 """
 import tensorflow as tf
+import pandas as pd
+import numpy as np
 
 
 def weight_variable(shape,name):
-    initial = tf.truncated_normal(shape, stddev=0.01)
+    initial = tf.truncated_normal(shape, stddev=0.1)
     return tf.Variable(initial,name=name)
 
 
@@ -15,11 +17,11 @@ def bias_variable(shape,name):
 
 
 def conv2d(x, W):
-    return tf.nn.conv2d(x, W, strides=[1, 0, 1, 1], padding='SAME')
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
 
 def max_pool_2x2(x):
-    return tf.nn.max_pool(x, ksize=[1, 1, 2, 1],strides=[1, 0, 2, 1], padding='SAME')
+    return tf.nn.max_pool(x, ksize=[1, 1, 2, 1],strides=[1, 1, 2, 1], padding='SAME')
 
 
 class CNN:
@@ -31,44 +33,54 @@ class CNN:
     def train(self, X, y):
         x = tf.placeholder("float", shape=[None, self.__input_dim])
         y_ = tf.placeholder("float", shape=[None, self.__output_dim])
-        W_conv1 = weight_variable([1, 5, 1, 32],'W_conv1')
+        W_conv1 = weight_variable([1, 25, 1, 32],'W_conv1')
         b_conv1 = bias_variable([32],'b_conv1')
         x_image = tf.reshape(x, [-1, 1, self.__input_dim, 1])
         h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
         h_pool1 = max_pool_2x2(h_conv1)
-        W_conv2 = weight_variable([1, 5, 128, 128], 'W_conv2')
-        b_conv2 = bias_variable([128], 'b_conv2')
+        W_conv2 = weight_variable([1, 12, 32, 32], 'W_conv2')
+        b_conv2 = bias_variable([32], 'b_conv2')
         h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
         h_pool2 = max_pool_2x2(h_conv2)
-        W_fc1 = weight_variable([7 * 7 * 128, 1024],'W_fc1')
-        b_fc1 = bias_variable([1024],'b_fc1')
-        h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*128])
+        W_fc1 = weight_variable([int(self.__input_dim/4) *32, 256],'W_fc1')
+        b_fc1 = bias_variable([256],'b_fc1')
+        h_pool2_flat = tf.reshape(h_pool2, [-1, int(self.__input_dim / 4)*32])
         h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
-        W_fc2 = weight_variable([1024, 10],'W_fc2')
+        W_fc2 = weight_variable([256, self.__output_dim],'W_fc2')
         keep_prob = tf.placeholder("float")
         h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-        b_fc2 = bias_variable([10],'b_fc2')
+        b_fc2 = bias_variable([self.__output_dim],'b_fc2')
         y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+        cross_entropy = -tf.reduce_sum(y * tf.log(y_conv + 1e-10), name='cross_entropy')#-tf.reduce_sum(y_*tf.log(y_conv),name='cross_entropy')
+        train_step = tf.train.AdamOptimizer(1e-3).minimize(cross_entropy)
+        correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"),name='accuracy')
+        all_vars = tf.trainable_variables()
+        for v in all_vars:
+            print(v.name)
+        saver = tf.train.Saver()
+        with tf.Session() as sess:
+            sess.run(tf.initialize_all_variables())
+            for i in range(20000):
+                if i%100 == 0:
+                    train_cross_entropy = sess.run(cross_entropy,feed_dict={x:X, y_: y, keep_prob: 1.0})
+                    train_accuracy = accuracy.eval(feed_dict={x:X, y_: y, keep_prob: 1.0})
+                    result = sess.run(y_conv,feed_dict={x:X, y_:y, keep_prob: 1.0})
+                    print(result)
+                    print ("step %d, training accuracy %g"%(i, train_accuracy))
+                    print (train_cross_entropy)
+                sess.run(train_step,feed_dict={x: X, y_: y, keep_prob: 1.0})
+            saver_path = saver.save(sess, "model/model_4.ckpt")
+            print ("Model saved in file: ", saver_path)
 
 
-cross_entropy = -tf.reduce_sum(y_*tf.log(y_conv),name='cross_entropy')
-train_step = tf.train.AdamOptimizer(3e-5).minimize(cross_entropy)
-correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"),name='accuracy')
-all_vars = tf.trainable_variables()
-for v in all_vars:
-    print(v.name)
-saver = tf.train.Saver()
-print(mnist.train.images.shape)
-with tf.Session() as sess:
-    sess.run(tf.initialize_all_variables())
-    for i in range(20000):
-        batch = mnist.train.next_batch(100)
-        if i%100 == 0:
-            train_cross_entropy = sess.run(cross_entropy,feed_dict={x:batch[0], y_: batch[1], keep_prob: 1.0})
-            train_accuracy = accuracy.eval(feed_dict={x:batch[0], y_: batch[1], keep_prob: 1.0})
-            print ("step %d, training accuracy %g"%(i, train_accuracy))
-            print (train_cross_entropy)
-        sess.run(train_step,feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
-    saver_path = saver.save(sess, "model/model_4.ckpt")
-    print ("Model saved in file: ", saver_path)
+if __name__ == "__main__":
+    X = pd.read_csv("data/single_mall_generated_feature/m_6167_2.csv", index_col=0)
+    y = pd.read_csv("data/second/m_6167_l.csv")
+    X = np.array(X)
+    y = np.array(pd.get_dummies(y, columns=["shop_num"]))
+    print(y)
+    input_dim = X.shape[1]
+    output_dim = y.shape[1]
+    cnn = CNN(input_dim, output_dim)
+    cnn.train(X,y)
---
+++ TPOT_train.py
@@ -0,0 +1,129 @@
+"""
+Created by Haochuan Lu on 10/24/17.
+"""
+import csv as csv
+import numpy as np
+import pandas as pd
+
+# 将特征分别读为features，labels
+name = "m_6167"
+features = np.loadtxt("data/second/" + name + "_f.csv", delimiter=",", skiprows=1)
+labels = np.loadtxt("data/second/" + name + "_l.csv", delimiter=",", skiprows=1)
+
+from tpot import TPOTClassifier
+
+tpot_config = {
+    'sklearn.ensemble.RandomForestClassifier': {
+        'n_estimators': [100],
+        'criterion': ["gini", "entropy"],
+        'max_features': np.arange(0.05, 1.01, 0.05),
+        'min_samples_split': range(2, 21),
+        'min_samples_leaf': range(1, 21),
+        'bootstrap': [True, False]
+    },
+
+    'sklearn.preprocessing.Binarizer': {
+        'threshold': np.arange(0.0, 1.01, 0.05)
+    },
+
+    'sklearn.decomposition.FastICA': {
+        'tol': np.arange(0.0, 1.01, 0.05)
+    },
+
+    'sklearn.cluster.FeatureAgglomeration': {
+        'linkage': ['ward', 'complete', 'average'],
+        'affinity': ['euclidean', 'l1', 'l2', 'manhattan', 'cosine', 'precomputed']
+    },
+
+    'sklearn.preprocessing.MaxAbsScaler': {
+    },
+
+    'sklearn.preprocessing.MinMaxScaler': {
+    },
+
+    'sklearn.preprocessing.Normalizer': {
+        'norm': ['l1', 'l2', 'max']
+    },
+
+    'sklearn.kernel_approximation.Nystroem': {
+        'kernel': ['rbf', 'cosine', 'chi2', 'laplacian', 'polynomial', 'poly', 'linear', 'additive_chi2', 'sigmoid'],
+        'gamma': np.arange(0.0, 1.01, 0.05),
+        'n_components': range(1, 11)
+    },
+
+    'sklearn.decomposition.PCA': {
+        'svd_solver': ['randomized'],
+        'iterated_power': range(1, 11)
+    },
+
+    'sklearn.preprocessing.PolynomialFeatures': {
+        'degree': [2],
+        'include_bias': [False],
+        'interaction_only': [False]
+    },
+
+    'sklearn.kernel_approximation.RBFSampler': {
+        'gamma': np.arange(0.0, 1.01, 0.05)
+    },
+
+    'sklearn.preprocessing.RobustScaler': {
+    },
+
+    'sklearn.preprocessing.StandardScaler': {
+    },
+
+    'tpot.builtins.ZeroCount': {
+    },
+
+    # Selectors
+    'sklearn.feature_selection.SelectFwe': {
+        'alpha': np.arange(0, 0.05, 0.001),
+        'score_func': {
+            'sklearn.feature_selection.f_classif': None
+        }
+    },
+
+    'sklearn.feature_selection.SelectPercentile': {
+        'percentile': range(1, 100),
+        'score_func': {
+            'sklearn.feature_selection.f_classif': None
+        }
+    },
+
+    'sklearn.feature_selection.VarianceThreshold': {
+        'threshold': np.arange(0.05, 1.01, 0.05)
+    },
+
+    'sklearn.feature_selection.RFE': {
+        'step': np.arange(0.05, 1.01, 0.05),
+        'estimator': {
+            'sklearn.ensemble.ExtraTreesClassifier': {
+                'n_estimators': [100],
+                'criterion': ['gini', 'entropy'],
+                'max_features': np.arange(0.05, 1.01, 0.05)
+            }
+        }
+    },
+
+    'sklearn.feature_selection.SelectFromModel': {
+        'threshold': np.arange(0, 1.01, 0.05),
+        'estimator': {
+            'sklearn.ensemble.ExtraTreesClassifier': {
+                'n_estimators': [100],
+                'criterion': ['gini', 'entropy'],
+                'max_features': np.arange(0.05, 1.01, 0.05)
+            }
+        }
+    }
+
+}
+
+tpot = TPOTClassifier(generations=2, population_size=10, verbosity=2,
+                      config_dict=tpot_config)
+tpot.fit(features, labels)
+
+"""tpot.export('m_1021.py')
+
+T_features = np.loadtxt(open("可改.csv","rb"),delimiter=",",skiprows=0)
+results = tpot.predict(T_features)
+np.savetxt('可改.csv', results, delimiter = ',')"""
